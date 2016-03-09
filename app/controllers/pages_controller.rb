df2b7704e28c0edd3380d1fbc6e5f8d23d7a3df0
Peter Jang <peter.x.jang@gmail.com>
2016-03-08 20:05:44 -0600

    added query params exercise solution

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,22 @@ class PagesController < ApplicationController
     @second_message = params[:green_llama]
     render 'query.html.erb'
   end
+
+  def guess
+    @name = params[:query_name].upcase
+    if @name[0] == "A"
+      @special_message = "Hey! Did you know that your name starts with the first letter in the alphabet? You probably did..."
+    end
+
+    winning_number = 32
+    user_guess = params[:guess].to_i
+    if user_guess == winning_number
+      @game_message = "You win!"
+    elsif user_guess > winning_number
+      @game_message = "You suck! (Too high)"
+    else
+      @game_message = "You suck! (Too low)"
+    end
+    render 'guess.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/query_example' => 'pages#query'
+  
+
+  get '/guessing_game' => 'pages#guess'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
